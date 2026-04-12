2d2175dc7f4f4caf3423bd83536d7fdc8722b23a
Aleksei Iakovlev <saint.alesha@ya.ru>
2026-04-12 22:30:39 +0300

    all tasks and tests are completed, but still there are improvmets ahead

--- src/bibip_car_service.py
+++ src/bibip_car_service.py
@@ -3,9 +3,13 @@ import os
 
 
 class CarService:
+
     def __init__(self, root_directory_path: str) -> None:
         self.root_directory_path = root_directory_path
         os.makedirs(root_directory_path, exist_ok=True)
+
+    RECORD_SIZE = 500
+    DATA_SIZE = RECORD_SIZE - 2 #(для учета \n)
 
     # Задание 1. Сохранение автомобилей и моделей
     def add_model(self, model: Model) -> Model:
@@ -13,12 +17,12 @@ class CarService:
         line_number = 0
 
         with open(f"{self.root_directory_path}/models.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(model.model_dump_json().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(model.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         with open(f"{self.root_directory_path}/models_index.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(model.index().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(model.index().ljust(self.DATA_SIZE) + "\n")
 
         return model
 
@@ -28,12 +32,12 @@ class CarService:
         line_number = 0
     
         with open(f"{self.root_directory_path}/cars.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(car.model_dump_json().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(car.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         with open(f"{self.root_directory_path}/cars_index.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(car.index().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(car.index().ljust(self.DATA_SIZE) + "\n")
 
         return car
 
@@ -43,12 +47,12 @@ class CarService:
         line_number = 0
 
         with open(f"{self.root_directory_path}/sales.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(sale.model_dump_json().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(sale.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         with open(f"{self.root_directory_path}/sales_index.txt", "a") as f:
-            f.seek(line_number * (500))
-            f.write(sale.index().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(sale.index().ljust(self.DATA_SIZE) + "\n")
         
         with open(f"{self.root_directory_path}/cars_index.txt", "r+") as f:
             line_number = -1
@@ -59,13 +63,13 @@ class CarService:
 
         with open(f"{self.root_directory_path}/cars.txt", "r+") as f:
 
-            f.seek(line_number * (500))
-            line = f.read(498)
+            f.seek(line_number * (self.RECORD_SIZE))
+            line = f.read(self.DATA_SIZE)
             car = Car.model_validate_json(line)
             car.status = CarStatus.sold
 
-            f.seek(line_number * (500))
-            f.write(car.model_dump_json().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(car.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         return car
 
@@ -123,7 +127,7 @@ class CarService:
                         sales_date=sale.sales_date,
                         sales_cost=sale.cost
                     )
-                except Exception:
+                except FileNotFoundError:
                     return None
                 
             else:
@@ -152,18 +156,18 @@ class CarService:
 
         with open(f"{self.root_directory_path}/cars.txt", "r+") as f:
 
-            f.seek(line_number * (500))
+            f.seek(line_number * (self.RECORD_SIZE))
             line = f.readline().strip()
             car = Car.model_validate_json(line)
             car.vin = new_vin
 
-            f.seek(line_number * (500))
-            f.write(car.model_dump_json().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(car.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         with open(f"{self.root_directory_path}/cars_index.txt", "r+") as f:
 
-            f.seek(line_number * (500))
-            f.write(car.index().ljust(498) + "\n")
+            f.seek(line_number * (self.RECORD_SIZE))
+            f.write(car.index().ljust(self.DATA_SIZE) + "\n")
 
         return car
         
@@ -190,29 +194,114 @@ class CarService:
         
         with open(f"{self.root_directory_path}/cars.txt", "r+") as f:
 
-            f.seek(car_line_number * (500))
+            f.seek(car_line_number * (self.RECORD_SIZE))
             line = f.readline().strip()
             car = Car.model_validate_json(line)
             car.status = CarStatus.available
 
-            f.seek(car_line_number * (500))
-            f.write(car.model_dump_json().ljust(498) + "\n")
+            f.seek(car_line_number * (self.RECORD_SIZE))
+            f.write(car.model_dump_json().ljust(self.DATA_SIZE) + "\n")
 
         with open(f"{self.root_directory_path}/sales.txt", "r+") as f:
 
-            f.seek(sale_line_number * (500))
+            f.seek(sale_line_number * (self.RECORD_SIZE))
             f.write("")
             f.truncate()
 
         with open(f"{self.root_directory_path}/sales_index.txt", "r+") as f:
 
-            f.seek(sale_line_number * (500))
+            f.seek(sale_line_number * (self.RECORD_SIZE))
             f.write("")
             f.truncate()
 
     # Задание 7. Самые продаваемые модели
     def top_models_by_sales(self) -> list[ModelSaleStats]:
-        raise NotImplementedError
+        
+        with open(f"{self.root_directory_path}/models_index.txt", "r") as f:
+            lis = []
+            for j in f:
+                lis.append(int(j.strip()))
+            models_sales = dict.fromkeys(lis, 0)
+        
+        with open(f"{self.root_directory_path}/sales.txt", "r") as f:
+            
+            for y in f:
+
+                sale = Sale.model_validate_json(y)
+                
+                with open(f"{self.root_directory_path}/cars_index.txt", "r+") as f:
+                    line_number = -1
+                    for i, line in enumerate(f):
+                        if sale.car_vin in line:
+                            line_number = i
+                            break
+
+                with open(f"{self.root_directory_path}/cars.txt", "r") as f:
+
+                    f.seek(line_number * (self.RECORD_SIZE))
+                    car_line = f.read(self.DATA_SIZE)
+                    car = Car.model_validate_json(car_line)
+
+                count = dict.pop(models_sales, car.model)
+                models_sales[car.model] = count + 1
+        
+        sorted_models_sales = dict(sorted(models_sales.items(), key=lambda item: item[1], reverse=True))
+        top_list = list(dict.keys(sorted_models_sales))
+
+        with open(f"{self.root_directory_path}/models_index.txt", "r") as f:
+
+            top_1_line_number = -1
+            top_2_line_number = -1
+            top_3_line_number = -1
+
+            for i, line in enumerate(f):
+                if top_list[0] == int(line):
+                    top_1_line_number = i
+                    continue
+                if top_list[1] == int(line):
+                    top_2_line_number = i
+                    continue
+                if top_list[2] == int(line):
+                    top_3_line_number = i
+                    continue
+
+        with open(f"{self.root_directory_path}/models.txt", "r") as f:
+
+            f.seek(top_1_line_number * (self.RECORD_SIZE))
+            line = f.read(self.DATA_SIZE)
+            top_1_model = Model.model_validate_json(line)
+
+            top_1 = ModelSaleStats(
+                car_model_name=top_1_model.name,
+                brand=top_1_model.brand,
+                sales_number=sorted_models_sales[top_list[0]]
+            )
+
+            f.seek(top_2_line_number * (self.RECORD_SIZE))
+            line = f.read(self.DATA_SIZE)
+            top_2_model = Model.model_validate_json(line)
+
+            top_2 = ModelSaleStats(
+                car_model_name=top_2_model.name,
+                brand=top_2_model.brand,
+                sales_number=sorted_models_sales[top_list[1]]
+            )
+
+            f.seek(top_3_line_number * (self.RECORD_SIZE))
+            line = f.read(self.DATA_SIZE)
+            top_3_model = Model.model_validate_json(line)
+
+            top_3 = ModelSaleStats(
+                car_model_name=top_3_model.name,
+                brand=top_3_model.brand,
+                sales_number=sorted_models_sales[top_list[2]]
+            )
+            
+        res = [top_1, top_2, top_3]
+
+        return res
+
+        
 
 #service = CarService('data')
 #sold = CarStatus.sold
@@ -220,3 +309,4 @@ class CarService:
 #print(service.get_car_info("5N1CR2TS0HW037674"))
 #res = service.update_vin("KNAGM4A77D5316538", "UPDGM4A77D5316538")
 #print(res)
+#print(service.top_models_by_sales())
